556987c32c7fdfa9fcbc59fcd527f64aa2a88e78
L Serj <gelvior@gmail.com>
2013-08-27 20:20:40 +0400

    Validations for Team and tests.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,22 @@
+# encoding: UTF-8
 class Team < ActiveRecord::Base
   attr_accessible :country_id, :name, :people_count, :price
 
   belongs_to :country
+
+  validates :name,
+            :presence => { :message => "Поле не должно быть пустым" },
+            :length   => { :maximum => 250, :message => "Поле должно содержать не более %{count} символов" }
+
+  validates :people_count,
+            :presence => { :message => "Поле не должно быть пустым" },
+            :numericality => { :only_integer => true,
+                               :greater_than => 0,
+                               :less_than_or_equal_to => 100 }
+
+  validates :price,
+            :presence => { :message => "Поле не должно быть пустым" },
+            :numericality => { :only_integer => true,
+                               :greater_than => 0,
+                               :less_than_or_equal_to => 100_000_000 }
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,77 @@
 require 'spec_helper'
 
 describe Team do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :name => "some text here", :people_count => 5, :price => 100 }
+  end
+
+  describe "DB" do
+    it "should create a new row with valid params" do
+      expect do
+        team = Team.create( @attr )
+      end.to change( Team, :count ).by( 1 )
+    end
+  end
+
+  describe "Validations for" do
+    it "name can't be empty" do
+      team = Team.create( @attr.merge :name => "" )
+      team.should_not be_valid
+    end
+
+    it "name can't contain > 250 symbols" do
+      team = Team.create( @attr.merge :name => "a" * 251 )
+      team.should_not be_valid
+    end
+
+    it "people_count should exists" do
+      team = Team.create( @attr.merge :people_count => nil )
+      team.should_not be_valid
+    end
+
+    it "people_count should be integer, not float" do
+      team = Team.create( @attr.merge :people_count => 1.10 )
+      team.should_not be_valid
+    end
+
+    it "people_count should be integer, not string" do
+      team = Team.create( @attr.merge :people_count => "abc" )
+      team.should_not be_valid
+    end
+
+    it "people_count should not accept 0" do
+      team = Team.create( @attr.merge :people_count => 0 )
+      team.should_not be_valid
+    end
+
+    it "people_count should not accept 101" do
+      team = Team.create( @attr.merge :people_count => 101 )
+      team.should_not be_valid
+    end
+
+    it "price should exists" do
+      team = Team.create( @attr.merge :price => nil )
+      team.should_not be_valid
+    end
+
+    it "price should be integer, not float" do
+      team = Team.create( @attr.merge :price => 1.01 )
+      team.should_not be_valid
+    end
+
+    it "price should be integer, not string" do
+      team = Team.create( @attr.merge :price => "abc" )
+      team.should_not be_valid
+    end
+
+    it "price should not accept 0" do
+      team = Team.create( @attr.merge :price => 0 )
+      team.should_not be_valid
+    end
+
+    it "price should not accept 100_000_001" do
+      team = Team.create( @attr.merge :price => 100_000_001 )
+      team.should_not be_valid
+    end
+  end
 end
