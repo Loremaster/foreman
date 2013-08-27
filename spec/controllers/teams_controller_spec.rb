8c1bbb337c7450b18009f4f1c4ba93094d2a76fd
L Serj <gelvior@gmail.com>
2013-08-27 21:39:53 +0400

    Tests (index, new).

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe TeamsController do
+  describe "GET 'index'" do
+    it "should access" do
+      get :index
+      response.should be_success
+    end
+  end
 
+  describe "GET 'new'" do
+    it "should access" do
+      get :new
+      response.should be_success
+    end
+  end
 end
