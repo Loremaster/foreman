d6e5088a630da1a0d5fe38eadbdd46c1be1b7f6c
L Serj <gelvior@gmail.com>
2013-08-28 19:14:33 +0400

    Add new tags from table in array (in params).

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,11 @@
 module TeamsHelper
+  def table_tags(tag)
+    if !params[:table_tags]                                                                         # if there is no array of tags.
+      link_to "#{tag} ", :table_tags => [tag]
+    else
+      tt = params[:table_tags].clone                                                                # Make clone (to avoid strange adding of another tags).
+      tt.push( tag ) unless tt.include? tag                                                         # Push only new tags.
+      link_to "#{tag} ", :table_tags => tt                                                          # Link to path with new array.
+    end
+  end
 end
