6a7fe999c3cd2705e6bc8038202c7ca95f5eb931
L Serj <gelvior@gmail.com>
2013-08-28 21:22:10 +0400

    Bugfix of arrow.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
-    css_class = column == sort_column ? "current #{sort_direction}" : nil
-    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    css_class = ( column == sort_column && ( params[:sort] && params[:table_tags] || !params[:table_tags]  ) ? "current #{sort_direction}" : nil )
+    direction = ( column == sort_column && sort_direction == "asc" ? "desc" : "asc" )
     link_to title, {:sort => column, :direction => direction, :country_id => params[:country_id], :tag => params[:tag], :table_tags => params[:table_tags]},
                    {:class => css_class}
   end
