35100e8894395c32a462c92a86958d66445c434c
L Serj <gelvior@gmail.com>
2013-08-29 13:25:15 +0400

    Fresh seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
+# encoding: UTF-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Team.create( name: "Blacksmith", people_count: 4, price: 454, country_id: 256, tag_list: "покраска, обои, побелка" )
+Team.create( name: "White", people_count: 34, price: 2350, country_id: 256, tag_list: "побелка" )
+Team.create( name: "Americano", people_count: 34, price: 4890, country_id: 263, tag_list: "покраска" )
+Team.create( name: "Black", people_count: 34, price: 554890, country_id: 256, tag_list: "покраска, обои" )
+Team.create( name: "Revolvero", people_count: 46, price: 345345, country_id: 257, tag_list: "обои, покраска" )
+Team.create( name: "Bambuko", people_count: 23, price: 23423423, country_id: 263, tag_list: "покраска, побелка, мелирование, размелирование" )
