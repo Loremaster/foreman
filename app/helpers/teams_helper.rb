31332440aef18cd33305c5b028f509eb44eba4aa
L Serj <gelvior@gmail.com>
2013-08-29 14:59:17 +0400

    Link to return to full list of teams.

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 module TeamsHelper
   # Adding new tags in array in params after clicking on chosen tag.
   def table_tags(tag)
@@ -8,5 +9,17 @@ module TeamsHelper
       tt.push( tag ) unless tt.include? tag                                                         # Push only new tags.
       link_to "#{tag} ", :table_tags => tt                                                          # Link to path with new array.
     end
+  end
+
+  def link_to_all_teams(*args)
+    default_val = nil
+
+    args.compact.each do |val|
+      if params.has_key? val
+        return link_to "← К полному спиcку", teams_path, :class => "all-teams-link"
+      end
+    end
+
+    default_val
   end
 end
